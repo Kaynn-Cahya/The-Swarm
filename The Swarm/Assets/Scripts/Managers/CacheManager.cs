e61d89f8e0538eb7640e665680f2ded00f4b0fe8
Kaynn Cahya <kaynncahya@outlook.com>
2020-05-11 08:51:28 +0900

    Added game cache

---
+++ The Swarm/Assets/Scripts/Managers/CacheManager.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using MyBox;
+
+namespace Managers {
+    public class CacheManager : MonoSingleton<CacheManager> {
+
+        internal GameCache<Enemy> EnemyCache { get; private set; }
+
+        protected override void OnAwake() {
+            EnemyCache = new GameCache<Enemy>();
+        }
+    }
+}
---
+++ The Swarm/Assets/Scripts/Utils/GameCache.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+internal class GameCache<T> where T : MonoBehaviour {
+    private readonly HashSet<T> cache;
+
+    internal GameCache(){
+        cache = new HashSet<T>();
+    }
+
+    internal void Add(T item) {
+        cache.Add(item);
+    }
+
+    internal void Remove(T item) {
+        cache.Remove(item);
+    }
+
+    internal bool TryFetchDisabledItem(out T disabledItem) {
+
+        return TryFetchByCondition(out disabledItem, ItemIsDisabled);
+
+        #region Local_Function
+
+#pragma warning disable IDE0062 // Make local function 'static'
+        bool ItemIsDisabled(T item) {
+#pragma warning restore IDE0062 // Make local function 'static'
+            return !item.gameObject.activeInHierarchy;
+        }
+
+        #endregion
+    }
+
+    internal bool TryFetchByCondition(out T outItem, Func<T, bool> condition) {
+        outItem = null;
+
+        foreach (var item in cache) {
+            if (condition(item)) {
+                outItem = item;
+                break;
+            }
+        }
+
+        return outItem != null;
+    }
+}
