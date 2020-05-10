1a33111ccbec33e96fc6aa67c5d4df709622dac5
JunRong <tanjunrong321@gmail.com>
2020-05-11 01:13:13 +0800

    Created menu

---
+++ The Swarm/Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour {
+
+	[SerializeField, Tooltip("Scene name to transition to.")] private string gameScene;
+	[SerializeField, Tooltip("Delay to transition to game scene.")] private float delay = 1f;
+
+	[SerializeField, Tooltip("Animator of start txt.")] private Animator startTxtAnimator;
+
+	private bool isTransitioning;
+
+	void Update() {
+		if(Input.anyKeyDown && !isTransitioning) {
+			isTransitioning = true;
+			StartCoroutine(GameSceneTransition());
+		}
+	}
+
+	private IEnumerator GameSceneTransition() {
+		startTxtAnimator.SetTrigger("GameStart");
+		yield return new WaitForSeconds(delay);
+		SceneManager.Instance.SwitchScene(gameScene);
+	}
+}
---
+++ The Swarm/Assets/Scripts/Managers/SceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using MyBox;
+
+public class SceneManager : MonoSingleton<SceneManager> {
+	protected override void OnAwake() {
+	}
+
+	public void SwitchScene(string sceneName) {
+		UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+	}
+}
