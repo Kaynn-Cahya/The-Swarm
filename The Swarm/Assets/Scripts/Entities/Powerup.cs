984565f864b08f69bbca2159f22bcd7cdf791f93
Kaynn Cahya <kaynncahya@outlook.com>
2020-05-11 13:00:13 +0900

    Added powerup

---
+++ The Swarm/Assets/Scripts/Entities/Powerup.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Entities {
+    [RequireComponent(typeof(Collider2D))]
+    public class Powerup : MonoBehaviour {
+
+        private void OnCollisionEnter2D(Collision2D other) {
+            if (other.gameObject.CompareTag("Player")) {
+                EnemyManager.Instance.KillAllEnemies();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ The Swarm/Assets/Scripts/Managers/PowerupManager.cs
@@ -0,0 +1,55 @@
+﻿using Entities;
+using MyBox;
+using UnityEngine;
+
+namespace Managers {
+    public class PowerupManager : MonoBehaviour {
+        [SerializeField, Tooltip("Intervals between each spawning of the powerup"), PositiveValueOnly]
+        private float spawnIntervals;
+
+        [SerializeField, Tooltip("Prefab for the powerup"), MustBeAssigned]
+        private Powerup powerupPrefab;
+
+        private Camera mainCamera;
+
+        private float spawnTimer;
+
+        private void Start() {
+            spawnTimer = 0f;
+            mainCamera = Camera.main;
+        }
+
+        private void Update() {
+            if (GameManager.Instance.GameOver) { return; }
+
+            spawnTimer += Time.deltaTime;
+
+            if (spawnTimer >= spawnIntervals) {
+                spawnTimer = 0f;
+                SpawnPowerup();
+            }
+        }
+
+        private void SpawnPowerup() {
+
+            Powerup powerup = Instantiate(powerupPrefab);
+            powerup.transform.position = GeneratePosition();
+
+            #region Local_Function
+
+            Vector2 GeneratePosition() {
+                Vector2 randPosition;
+
+                // Generate random position on screen.
+                // Ensure position is not near the player.
+                do {
+                    randPosition = mainCamera.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+                } while (Vector2.Distance(Player.Instance.transform.position, randPosition) <= 1);
+
+                return randPosition;
+            }
+
+            #endregion
+        }
+    }
+}
